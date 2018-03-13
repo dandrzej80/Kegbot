73421a12f4ffdfcd2d1a628fdae0ce6c61755968
dandrzej80 <dandrzej80@gmail.com>
2018-03-13 14:00:14 -0400

    Restructure

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+#General Setup Script For Raspian Install
+
+#Get Generic Install Script from GitHub
+git clone https://github.com/dandrzej80/Raspi-Scripts.git
+
+#Run Generic Scripts
+cd Raspi-Scripts
+sudo bash basic-setup.sh
+cd ..
+
+#Install KegBot and Dependencies
+set -e
+set -x
+sudo bash -c "DEBIAN_FRONTEND=noninteractive apt-get -yq install python-setuptools"
+sudo easy_install pip
+sudo pip install -U virtualenv
+sudo pip install -U --pre kegberry
+sudo mv ~/Kegbot/app.py /usr/local/lib/python2.7/dist-packages/kegberry.app.py
+kegberry $INSTALLFLAGS install
