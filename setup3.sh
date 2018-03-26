bb59de84414b1466c17037a362a2b1b606db164e
dandrzej80 <dandrzej80@gmail.com>
2018-03-26 12:30:53 -0400

    install scripts

--- setup3.sh
+++ setup3.sh
@@ -3,6 +3,14 @@
 
 #Get Generic Install Script from GitHub
 #git clone https://github.com/dandrzej80/Raspi-Scripts.git
+sudo apt-get install tightvncserver -y
+vncserver #-1234 -1234 -n
+mv tightvncserver /etc/init.d
+sudo chmod 755 /etc/init.d/tightvncserver
+sudo update-rc.d tightvncserver defaults
+
+cd
+sudo ed -s /etc/crontab <<< $'$-1 r ./Raspi-Scripts/edit-crontab\nw\nq\n'
 
 #Run Generic Scripts
 #cd Raspi-Scripts
